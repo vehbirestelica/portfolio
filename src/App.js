28b596cae75e5b850a75cf42faed67b7a9197a29
Vehbi Restelica <vehb.res@gmail.com>
2021-03-02 18:35:38 +0100

    Small Changes

--- src/App.js
+++ src/App.js
@@ -4,15 +4,11 @@ import Home from "./pages";
 
 function App() {
   return (
-    // <Router>
-    //   <Switch>
-    //     <Route path="/" component={Home} exact />
-    //   </Switch>
-    // </Router>
-    <div className="App">
-      <h1>Hi</h1>
-      <h2>hwhwhwhwhw</h2>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" component={Home} exact />
+      </Switch>
+    </Router>
   );
 }
 
