c6325fec4372c5eb26f64bd95edfd9d9c124d9d3
Vehbi Restelica <vehb.res@gmail.com>
2021-03-02 18:25:11 +0100

    Small Changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import './App.css';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Home from './pages';
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./pages";
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/" component={Home} exact />
-      </Switch>
-    </Router>
+    // <Router>
+    //   <Switch>
+    //     <Route path="/" component={Home} exact />
+    //   </Switch>
+    // </Router>
+    <div className="App">
+      <h1>Hi</h1>
+      <h2>hwhwhwhwhw</h2>
+    </div>
   );
 }
 
